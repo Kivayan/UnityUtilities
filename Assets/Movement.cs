745325de9109ec7e191449edbbbe2554f4b5bc3d
KIV-LAPTOP\Kivayan <kivayan@gmail.com>
2017-03-17 20:19:38 +0100

    Innitial Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void DetectMovement()
+    {
+        if (rb.velocity == Vector3.zero)
+            anim.SetBool("isMoving", false);
+
+        else
+            anim.SetBool("isMoving", true);
+    }
+
+}
