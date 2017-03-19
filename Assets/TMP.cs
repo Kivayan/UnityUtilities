85002ad66300191bc797c3f9e190fee09eee78ed
KIV-LAPTOP\Kivayan <kivayan@gmail.com>
2017-03-19 19:50:05 +0100

    TMP innitiated to store loose bits of code

---
+++ Assets/TMP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TMP : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    private void PopulateArray()
+    {
+        waypoints = new Transform[transform.childCount];
+        int i = 0;
+
+        foreach (Transform t in transform)
+        {
+            waypoints[i++] = t;
+        }
+    }
+}
+}
