dc20c6ae12c423bdf361a536663f6cf38d6fc4b6
KIV-LAPTOP\Kivayan <kivayan@gmail.com>
2017-03-22 19:45:38 +0100

    Added DebugPanel Extension

---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private float Score;
+    private float dupa;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Score = 0;
+        dupa = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        Debug();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Score++;
+            
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            dupa+=10;
+            
+        }
+    }
+
+    private void Debug()
+    {
+        DebugPanel.Log("Score", "DupaKwas", Score);
+        DebugPanel.Log("Dupa", "DupaKwas", dupa);
+    }
+}
