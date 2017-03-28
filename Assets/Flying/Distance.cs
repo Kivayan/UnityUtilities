7109f034057d0645467e21341e66f48cb03c62f7
KIV-LAPTOP\Kivayan <kivayan@gmail.com>
2017-03-28 19:54:51 +0200

    Tracking distance from ground working

---
+++ Assets/Flying/Distance.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Distance : MonoBehaviour
+{
+    private RaycastHit rayHit;
+    private Ray ray;
+
+    private float distance;
+
+    private void Start()
+    {
+        ray = new Ray(transform.position, Vector3.down);
+    }
+
+    private void Update()
+    {
+        CalculateDist();
+        DebugInfo();
+        
+    }
+
+    private void DebugInfo()
+    {
+        DebugPanel.Log("Distance", "Distance", distance);
+    }
+
+    private void CalculateDist()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out rayHit))
+        {
+            distance = rayHit.distance;
+        }
+        Debug.DrawRay(transform.position, Vector3.down);
+    }
+}
